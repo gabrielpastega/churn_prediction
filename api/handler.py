25c637a44256a59c398b02024beaa4f3246de73d
gabrielpastega <88066683+gabrielpastega@users.noreply.github.com>
2023-06-08 19:43:39 -0300

    Update handler.py

--- api/handler.py
+++ api/handler.py
@@ -1,6 +1,7 @@
 import pickle
 import pandas as pd
 import numpy  as np
+import os
 
 from flask                           import Flask, request, Response
 from churnprediction.ChurnPrediction import ChurnPrediction
