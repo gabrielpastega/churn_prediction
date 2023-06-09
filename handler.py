5551ee5ce418f008b84cffe3e164d627e3d1d56a
gabrielpastega <88066683+gabrielpastega@users.noreply.github.com>
2023-06-09 12:36:01 -0300

    Update handler.py

--- handler.py
+++ handler.py
@@ -52,5 +52,5 @@ def churn_prediction():
         return Response('{}', status=200, mimetype='application/json')
     
 if __name__ == '__main__':
-    port = os.environ.get('PORT', 5000)
-    app.run(host='0.0.0.0', port=port)
+    #port = os.environ.get('PORT', 5000)
+    app.run(host='0.0.0.0')
